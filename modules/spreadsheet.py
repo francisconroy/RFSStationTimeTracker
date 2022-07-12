d4ed9875b74a997396af584da2b8277c6c03e4e7
Francis Conroy <francisc@posteo.net>
2022-07-12 20:04:12 +1000

    Start of spreadsheet module

---
+++ modules/spreadsheet.py
@@ -0,0 +1,48 @@
+import os
+
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = 'YVt05Wy1beb5UrsWdKHm4qM9bBOCKvhBmIss8BA'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+
+service = None
+
+
+def setup_service():
+    global service
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+    except HttpError as err:
+        print(err)
+
+
+def get_employee_list():
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return
+
+    print('Name, Major:')
+    for row in values:
+        # Print columns A and E, which correspond to indices 0 and 4.
+        print('%s, %s' % (row[0], row[4]))
